07dcecff9a47ef7ba07a0076d2c59953419f6864
mikeyangh <472457791@qq.com>
2017-02-06 14:08:44 -0800

    Add description

--- github-battle/app/components/Home.js
+++ github-battle/app/components/Home.js
@@ -8,7 +8,7 @@ var Home = React.createClass({
 		return (
 			<div className='jumbotron col-sm-12 text-center' style={transparentBg}>
 				<h1>Github Battle</h1>
-				<p className='lead'>Some fancy motto</p>
+				<p className='lead'>Compare Github Popularity easily!</p>
 				<Link to='/playerOne'>
 					<button type='button' className='btn btn-lg btn-success'>Get Started</button>
 				</Link>
